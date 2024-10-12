41a8a7f6c68bff09c3b4d9a5bcfa28ae2a4d93f3
Salar <90781984+SALSAL1994@users.noreply.github.com>
2024-10-12 16:01:24 +0200

    update

--- Front/src/app/home/home.component.ts
+++ Front/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import { Location } from '@angular/common'; // Import Location service
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,32 @@ import { UserService } from '../_services/user.service';
 export class HomeComponent implements OnInit {
   content?: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private location: Location) { }
 
   ngOnInit(): void {
+    // Check if the page has already been refreshed
+    if (!localStorage.getItem('reloaded')) {
+      localStorage.setItem('reloaded', 'true');  // Set a flag in localStorage
+      this.location.go(this.location.path());    // Reload the current path
+      window.location.reload();                  // Trigger reload
+    } else {
+      localStorage.removeItem('reloaded');       // Reset the flag after refresh
+
+      // Fetch the public content after reload
+      this.userService.getPublicContent().subscribe({
+        next: data => {
+          this.content = data;
+        },
+        error: err => {
+          console.log(err);
+          if (err.error) {
+            this.content = JSON.parse(err.error).message;
+          } else {
+            this.content = "Error with status: " + err.status;
+          }
+        }
+      });
+    }
     this.userService.getPublicContent().subscribe({
       next: data => {
         this.content = data;
